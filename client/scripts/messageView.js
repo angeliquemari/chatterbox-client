774196ad3ea7e76865f472c45cab158398a4da8c
angeliquemari <angelique@nehmzow.com>
2019-09-13 17:10:50 -0700

    fixing this binding for message render function

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,35 +1,13 @@
 var MessageView = {
 
   render: function () {
-    var obj = this;
     return _.template(`
       <div class="chat">
-        <div class="username">${obj.username}</div>
-        <div class="text">${obj.text}</div>
-        <div class="text">${obj.createdAt}</div>
+        <div class="username">${this.username}</div>
+        <div class="text">${this.text}</div>
+        <div class="text">${this.createdAt}</div>
       </div>
     `);
   }
-
-  // render: function (chat) {
-  //   var template = _.template(`
-  //       <div class="chat">
-  //         <div class="username">${username}</div>
-  //         <div class="text">${text}</div>
-  //         <div class="createdAt">${createdAt}</div>
-  //       </div>
-  //     `)
-  //     return template(chat);
-  //   }
-  // function() {
-  //   console.log(`
-  //   <div class="chat">
-  //     <div class="username">${this.username}</div>
-  //     <div class="text">${this.text}</div>
-  //     <div class="text"><%-${this.createdAt}%></div>
-  //   </div>
-  // `);
-  // }
-
 
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -8,17 +8,13 @@ var MessagesView = {
   render: function(data) {
     // loop through chats (results array), for each, call messageView.render
     // that will append an html snippet with info about specific chat
-    console.log(data);
-    // for (var i = 0; i < data.results.length; i++) {
-    // for (var i = 0; i < 3; i++) {
-    // var chat = data.results[i];
-    var chat = data.results[2];
-    console.log(chat);
-    chat.render = MessageView.render;
-    var chatHTMLSnippet = chat.render();
-    // var chatHTMLSnippet = MessageView.render(chat);
-    this.$chats.append(chatHTMLSnippet);
-    // }
+    // console.log(data);
+    for (var i = 0; i < data.results.length; i++) {
+      var chat = data.results[i];
+      // console.log(chat);
+      var chatHTMLSnippet = MessageView.render.bind(chat);
+      this.$chats.append(chatHTMLSnippet);
+    }
   }
 
 };
